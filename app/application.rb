c9cae7d7a1e8ed6685fbd868b579457145a8e686
David Kuruvilla <dapawn@gmail.com>
2018-01-19 14:22:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
- 
+
   @@items = []
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
- 
+
       item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
       if (item = @@items.find{|i| i.name == item_name} )
         resp.write item.price
