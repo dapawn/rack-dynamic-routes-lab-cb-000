52b1d85fb625d4a56ec7451393b013343c797c99
David Kuruvilla <dapawn@gmail.com>
2018-01-19 14:18:19 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+ 
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+ 
+      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+        if (item = @@items.find{|i| i.name == item_name} )
+          resp.write item.price
+        else
+          resp.write "Item not found"
+        end
+    end
+ 
+    resp.finish
+  end
+end
